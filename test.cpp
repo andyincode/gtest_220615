2ae4d891f37342ed181af7ee57c63549d1e37458
Chansik Yun <chansigi@gmail.com>
2022-06-15 04:10:18 +0000

    .

---
+++ test.cpp
@@ -0,0 +1,17 @@
+
+// test.cpp
+
+#include <gtest/gtest.h>
+
+// Google Test 에서 테스트케이스를 만드는 방법
+// TEST(TestSuite, TestCase) {}
+
+TEST(SampleTest, FirstTest)
+{
+
+}
+
+TEST(SampleTest, SecondTest)
+{
+
+}
