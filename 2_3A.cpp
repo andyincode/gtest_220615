14be4a28c52ed20dcd560c8725f7400ee78ff5b7
Chansik Yun <chansigi@gmail.com>
2022-06-15 06:34:30 +0000

    .

--- 2_3A.cpp
+++ 2_3A.cpp
@@ -27,6 +27,17 @@ public:
 //    1) Arrange: 테스트 대상 코드를 초기화하고, 필요한 경우 설정하고 준비합니다.
 //    2) Act: 테스트 대상 코드에 작용을 가한다.(함수를 호출한다)
 //    3) Assert: 기대하는 바를 단언한다.
+//
+// 3. 테스트 코드 품질
+//    1) 가독성
+//     - 테스트케이스의 시나리오가 제대로 드러나는가?
+//     - 테스트케이스가 실패하였을 때 실패의 원인이 쉽게 드러나는가?
+//    2) 유지보수성
+//     - 테스트케이스에서 오류가 발생할 수 없는 형태의 코드를 지향해야 합니다.
+//      > 제어 구문(조건문, 반복문, 예외 처리 등) 검증
+//      > 테스트케이스 코드 안에서 제어 구문을 지양해야 합니다.
+//    3)
+
 
 TEST(CalcTest, PressPlus_TwoPlusTwoEquals_DisplaysFour) {
 	// 1. Arrange
@@ -39,11 +50,19 @@ TEST(CalcTest, PressPlus_TwoPlusTwoEquals_DisplaysFour) {
 	calc->PressEquals();
 
 	// 3. Assert
+/*
 	if (calc->Display() == 4) {
 		SUCCEED();
 	} else {
-		FAIL();
+		// FAIL();
+		FAIL() << "2+2를 하였을 때 4가 나오지 않음.";
 	}
+*/
+	// 위의 형태로 검증하는 것이 아니라, 단위 테스트 프레임워크가 제공하는
+	// 단언문을 사용해야 합니다.
+	//  => 함수, 매크로
+	//  ASSERT_EQ / LT / GT / NE / LE / GE / NULL / NOT_NULL
+	ASSERT_EQ(calc->Display(), 4) << "2 + 2 하였을 때";
 }
 
 
