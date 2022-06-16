17b9f0d91993e456ac52898b864549b8d076709c
Chansik Yun <chansigi@gmail.com>
2022-06-16 00:51:07 +0000

    .

--- 4_스위트픽스쳐.cpp
+++ 4_스위트픽스쳐.cpp
@@ -18,6 +18,7 @@ public:
 //-------------------------
 #include <gtest/gtest.h>
 
+#if 0
 class TerminalTest : public testing::Test
 {
 protected:
@@ -38,6 +39,7 @@ TEST_F(TerminalTest, Login)
   ASSERT_TRUE(terminal->IsLogin()) << "로그인하였을 때";
 
   // 해체
+  terminal->Disconnect();
   delete terminal;
 }
 
@@ -55,5 +57,40 @@ TEST_F(TerminalTest, Logout)
   ASSERT_FALSE(terminal->IsLogin()) << "로그아웃 하였을 때";
 
   // 해체
+  terminal->Disconnect();
   delete terminal;
+}
+#endif
+
+class TerminalTest : public testing::Test
+{
+protected:
+  Terminal *terminal = nullptr;
+
+  void SetUp() override
+  {
+    terminal = new Terminal;
+    terminal->Connect();
+  }
+
+  void TearDown() override
+  {
+    terminal->Disconnect();
+    delete terminal;
+  }
+};
+
+TEST_F(TerminalTest, Login)
+{
+  terminal->Login("test_id", "test_password");
+
+  ASSERT_TRUE(terminal->IsLogin()) << "로그인하였을 때";
+}
+
+TEST_F(TerminalTest, Logout)
+{
+  terminal->Login("test_id", "test_password");
+  terminal->Logout();
+
+  ASSERT_FALSE(terminal->IsLogin()) << "로그아웃 하였을 때";
 }
