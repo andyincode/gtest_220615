0f3c5e9f9033bd04d518addde4e457ccff5976e2
Chansik Yun <chansigi@gmail.com>
2022-06-16 00:41:57 +0000

    .

--- 3_테스트픽스쳐5.cpp
+++ 3_테스트픽스쳐5.cpp
@@ -15,12 +15,35 @@ protected:
 
 TEST_F(ImageProcessorTest, ResizeImage) {}
 TEST_F(ImageProcessorTest, BlurImage) {}
+TEST_F(ImageProcessorTest, BlurImage2) {}
+
+// 1. TestSuite 클래스의 객체는 모든 TestCase 마다 새롭게 생성되고 파괴됩니다.
+//  => xUnit Test Framework은 "신선한 픽스쳐의 전략"을 사용하고 있습니다.
+//  => 픽스쳐가 이전의 테스트케이스를 통해서 문제가 생겨도, 이후의 테스트케이스에는 영향을 주지 않는
+//     전략입니다.
+//  => 테스트는 독립적으로 구성될 수 있습니다.
+
+// 2. 테스트케이스를 구성하는 방법
+//   xUnit Test Pattern => 4단계 테스트 패턴(Four Phase Test Pattern)
+//   1단계: 테스트 픽스처를 설치하거나 실제 결과를 관찰하기 위해 필요하는 것을 설정하는 작업을 수행합니다.
+//         => SetUp()
+//   2단계: SUT와 상호작용 합니다.  => TestBody()
+//   3단계: 기대 결과를 확인합니다.  => TestBody()
+//   4단계: 테스트 픽스쳐를 해체해서, 테스트 시작 이전의 상태로 돌려놓습니다.
+//        => TearDown()
+
+// 3. Test Suite
+//  : A test suite is a set of tests that all share the same fixture
+//  => 동일한 픽스쳐를 공유하는 테스트 케이스의 집합입니다.
 
 // ImageProcessorTest* test = new ImageProcessorTest;
 // ImageProcessorTest.ResizeImage
-//  SetUp()
-//  TearDown()
+//  test->SetUp()
+//  test->TearDown()
+// delete test;
+
+// ImageProcessorTest* test = new ImageProcessorTest;
 // ImageProcessorTest.BlurImage
-//  SetUp()
-//  TearDown()
+//  test->SetUp()
+//  test->TearDown()
 // delete test;
