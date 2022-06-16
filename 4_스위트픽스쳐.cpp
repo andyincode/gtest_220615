abd3d480e62bc7a6e3e5ff8edb1bdcf36bab9daa
Chansik Yun <chansigi@gmail.com>
2022-06-16 00:47:59 +0000

    .

---
+++ 4_스위트픽스쳐.cpp
@@ -0,0 +1,59 @@
+// 4_스위트픽스쳐.cpp
+#include <string>
+#include <unistd.h> // sleep
+
+class Terminal
+{
+public:
+  // Connect / Disconnect가 느리다고 가정합니다.
+  void Connect() { sleep(2); }
+  void Disconnect() { sleep(1); }
+
+  void Login(const std::string &id, const std::string &password) {}
+  void Logout() {}
+
+  bool IsLogin() const { return false; }
+};
+
+//-------------------------
+#include <gtest/gtest.h>
+
+class TerminalTest : public testing::Test
+{
+protected:
+  void SetUp() override {}
+  void TearDown() override {}
+};
+
+TEST_F(TerminalTest, Login)
+{
+  // Arrange
+  Terminal *terminal = new Terminal;
+  terminal->Connect();
+
+  // Act
+  terminal->Login("test_id", "test_password");
+
+  // Assert
+  ASSERT_TRUE(terminal->IsLogin()) << "로그인하였을 때";
+
+  // 해체
+  delete terminal;
+}
+
+TEST_F(TerminalTest, Logout)
+{
+  // Arrange
+  Terminal *terminal = new Terminal;
+  terminal->Connect();
+
+  // Act
+  terminal->Login("test_id", "test_password");
+  terminal->Logout();
+
+  // Assert
+  ASSERT_FALSE(terminal->IsLogin()) << "로그아웃 하였을 때";
+
+  // 해체
+  delete terminal;
+}
