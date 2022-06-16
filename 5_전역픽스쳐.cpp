038a1dcd0f5f30f2fc41bcb7455454e367e63678
Chansik Yun <chansigi@gmail.com>
2022-06-16 01:38:33 +0000

    .

---
+++ 5_전역픽스쳐.cpp
@@ -0,0 +1,47 @@
+// 5_전역픽스쳐.cpp
+//  : Global Fixture SetUp / TearDown
+// => xUnit Test Framework이 제공하는 공통의 기능이 아니라,
+//    Google Test 고유의 기능입니다.
+// => 프로그램의 시작 단계에서 초기화해야 하는 작업과,
+//    프로그램의 종료 단계에서 정리해야 하는 작업을 정의합니다.
+
+#include <gtest/gtest.h>
+
+class SampleTest : public testing::Test
+{
+protected:
+  void SetUp() override { printf("SetUp()\n"); }
+  void TearDown() override { printf("TearDown()\n"); }
+  static void SetUpTestSuite() { printf("SetUpTestSuite()\n"); }
+  static void TearDownTestSuite() { printf("TearDownTestSuite()\n"); }
+};
+
+TEST_F(SampleTest, FirstTest) {}
+TEST_F(SampleTest, SecondTest) {}
+
+TEST(SampleTest2, foo) {}
+TEST(SampleTest3, foo) {}
+
+class TestEnvironment : public testing::Environment
+{
+public:
+  TestEnvironment() { printf("TestEnvironment()\n"); }
+  ~TestEnvironment() { printf("~TestEnvironment()\n"); }
+
+  void SetUp() override { printf("TestEnvironment::SetUp()\n"); }
+  void TearDown() override { printf("TestEnvironment::TearDown()\n"); }
+};
+
+// Global Fixture를 설치하는 방법 2가지
+// 1) 사용자가 main을 제공하고 있지 않을 때
+// 2) 사용자가 main을 제공하고 있을 때
+
+int main(int argc, char **argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+
+  testing::AddGlobalTestEnvironment(new TestEnvironment);
+  // 주의사항: 반드시 new를 통해 제공해야 합니다.
+
+  return RUN_ALL_TESTS();
+}
