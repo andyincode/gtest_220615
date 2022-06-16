b3c96a6991570ad9e29d35ab19c18f7760c9e28a
Chansik Yun <chansigi@gmail.com>
2022-06-16 01:15:51 +0000

    .

--- 4_스위트픽스쳐2.cpp
+++ 4_스위트픽스쳐2.cpp
@@ -18,90 +18,38 @@ public:
 //-------------------------
 #include <gtest/gtest.h>
 
-#if 0
 class TerminalTest : public testing::Test
 {
 protected:
-  void SetUp() override {}
-  void TearDown() override {}
-};
-
-TEST_F(TerminalTest, Login)
-{
-  // Arrange
-  Terminal *terminal = new Terminal;
-  terminal->Connect();
-
-  // Act
-  terminal->Login("test_id", "test_password");
-
-  // Assert
-  ASSERT_TRUE(terminal->IsLogin()) << "로그인하였을 때";
-
-  // 해체
-  terminal->Disconnect();
-  delete terminal;
-}
-
-TEST_F(TerminalTest, Logout)
-{
-  // Arrange
-  Terminal *terminal = new Terminal;
-  terminal->Connect();
-
-  // Act
-  terminal->Login("test_id", "test_password");
-  terminal->Logout();
-
-  // Assert
-  ASSERT_FALSE(terminal->IsLogin()) << "로그아웃 하였을 때";
-
-  // 해체
-  terminal->Disconnect();
-  delete terminal;
-}
-#endif
-
-class TerminalTest : public testing::Test
-{
-protected:
-  Terminal *terminal = nullptr;
+  // Terminal *terminal = nullptr;
+  static Terminal *terminal;
 
   void SetUp() override
   {
-    terminal = new Terminal;
-    terminal->Connect();
   }
 
   void TearDown() override
   {
-    terminal->Disconnect();
-    delete terminal;
   }
 
   // Suite Fixture SetUp() / TearDown()
   static void SetUpTestSuite()
   {
     printf("SetUpTestSuite()\n");
+    terminal = new Terminal;
+    terminal->Connect();
   }
 
   static void TearDownTestSuite()
   {
     printf("TearDownTestSuite()\n");
+    terminal->Disconnect();
+    delete terminal;
   }
-
-#if 0
-  static void SetUpTestCase()
-  {
-    printf("SetUpTestCase()\n");
-  }
-
-  static void TearDownTestCase()
-  {
-    printf("TearDownTestCase()\n");
-  }
-#endif
 };
+
+// 정적 멤버 변수는 외부 정의가 필요합니다 - C++ 문법
+Terminal *TerminalTest::terminal = nullptr;
 
 TEST_F(TerminalTest, Login)
 {
