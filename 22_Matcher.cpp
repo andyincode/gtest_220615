e0353a75966e5c0da1b7d629a7664f201afafe5d
Chansik Yun <chansigi@gmail.com>
2022-06-17 06:25:50 +0000

    .

--- 22_Matcher.cpp
+++ 22_Matcher.cpp
@@ -12,6 +12,7 @@ int GetAge() { return 42; }
 const char *GetMessage() { return "Hello, world"; }
 
 using testing::Eq; // ==
+
 using testing::MatchesRegex;
 using testing::StartsWith;
 
@@ -19,6 +20,38 @@ TEST(HamcrestTest, Sample1) {
   EXPECT_EQ(GetAge(), 42);
 
   EXPECT_THAT(GetAge(), Eq(42));
-  EXPECT_THAT(GetMessage(), StartsWith("xHello"));
-  EXPECT_THAT(GetMessage(), MatchesRegex("x.*"));
+  EXPECT_THAT(GetMessage(), StartsWith("Hello"));
+  EXPECT_THAT(GetMessage(), MatchesRegex(".*"));
+}
+
+class User {
+  std::string name;
+  int age;
+
+public:
+  User(const std::string &name, int age) : name(name), age(age) {}
+
+  std::string GetName() const { return name; }
+  int GetAge() const { return age; }
+
+  FRIEND_TEST(UserTest, Sample1);
+};
+
+using testing::Field;
+using testing::Lt;
+using testing::Property;
+
+std::ostream &operator<<(std::ostream &os, const User &user) {
+  return os << "User{name=" << user.GetName() << "}";
+}
+
+TEST(UserTest, Sample1) {
+  User user("Bob", 42);
+  ///...
+
+  EXPECT_EQ(user.GetName(), "Tom");
+  EXPECT_EQ(user.GetAge(), 42);
+
+  EXPECT_THAT(user, Property(&User::GetName, Eq("Tom")));
+  EXPECT_THAT(user, Field(&User::age, Lt(10))) << "연산하였을 때";
 }
